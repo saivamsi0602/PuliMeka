dcdf66cc855228d2e9cbe66cebd16dd6b689a6d3
Vamsi <saivamsi0602@outlook.com>
2021-08-04 21:07:18 +0530

    Added player in shared

---
+++ Shared/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PuliMeka.Shared
+{
+    public class Player
+    {
+        [EmailAddress(ErrorMessage ="Email Address is not in the correct format")]
+        [Required(ErrorMessage ="Email Address field is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage ="UserName is required")]
+        public string UserName { get; set; }
+    }
+}
